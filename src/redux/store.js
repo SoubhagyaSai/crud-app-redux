6334cdfe9b1621260c9c424999c5c822a578d9af
Soubhagya Sai Das <106137904+SoubhagyaSai@users.noreply.github.com>
2024-10-12 19:55:15 +0530

    Update store.js userReducer to userSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../features/userSlice'
+import userSlice from '../features/userSlice'
 
 export const store = configureStore({
   reducer: {
-    users: userReducer
+    users: userSlice
   },
 })
